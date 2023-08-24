031d420d126203e39dfeb197b7c4239fb3874ac5
Rutuja Dond <dondrutuja26@gmail.com>
2023-08-24 21:21:23 +0530

    end

--- index.js
+++ index.js
@@ -1,16 +1,8 @@
 import jsonServer from "json-server";
 import cors from "cors";
 import path from "path";
-import fs from "fs";
 import { fileURLToPath } from "url";
 import { dirname } from "path";
-
-const data = {
-  key: "value",
-};
-
-const jsonData = JSON.stringify(data, null, 2);
-const filePath = "./db.json";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -24,16 +16,8 @@ server.use(jsonServer.bodyParser);
 server.use(middlewares);
 server.use(router);
 
-fs.writeFile(filePath, jsonData, "utf8", (err) => {
-  if (err) {
-    console.error("Error writing to file:", err);
-  } else {
-    console.log("Data written to file successfully.");
-  }
-});
-
-const PORT = 3000;
+const PORT = 8000;
 
 server.listen(PORT, () => {
-  console.log(`JSON Server is running on :${PORT}`);
+  console.log(`JSON Server is running on http://localhost:${PORT}`);
 });
