ca3f34b7f0aff7207e91071b4efe685695d0310a
Rutuja Dond <dondrutuja26@gmail.com>
2023-08-25 10:29:07 +0530

    check

--- index.js
+++ index.js
@@ -1,20 +1,47 @@
+import AWS from "aws-sdk";
 import jsonServer from "json-server";
 import cors from "cors";
-import path from "path";
-import { fileURLToPath } from "url";
-import { dirname } from "path";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 
 const server = jsonServer.create();
-const router = jsonServer.router(path.join(__dirname, "db.json"));
 const middlewares = jsonServer.defaults();
- 
+
 server.use(cors());
 server.use(jsonServer.bodyParser);
 server.use(middlewares);
-server.use(router);
+
+// Initialize DynamoDB
+AWS.config.update({ region: "your-region" });
+const dynamoDB = new AWS.DynamoDB.DocumentClient();
+const tableName = "your-dynamodb-table-name";
+
+// Define custom routes
+server.get("/data", async (req, res) => {
+  try {
+    const params = {
+      TableName: tableName,
+    };
+    const result = await dynamoDB.scan(params).promise();
+    res.json(result.Items);
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    res.status(500).json({ error: "Error fetching data" });
+  }
+});
+
+server.post("/data", async (req, res) => {
+  try {
+    const newItem = req.body;
+    const params = {
+      TableName: tableName,
+      Item: newItem,
+    };
+    await dynamoDB.put(params).promise();
+    res.status(201).json(newItem);
+  } catch (error) {
+    console.error("Error adding data:", error);
+    res.status(500).json({ error: "Error adding data" });
+  }
+});
 
 const PORT = 3000;
 
