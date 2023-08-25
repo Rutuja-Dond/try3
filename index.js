79e509d927a15862b4340411f6f4e16fa7a9700f
Rutuja Dond <dondrutuja26@gmail.com>
2023-08-25 09:35:14 +0530

    last commit

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const data = {
   key: "value",
 };
 
-const filePath = "path/to/db.json";
+const filePath = "./db.json";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
