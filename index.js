616baa7871a9108b02d163e9975bd12f7623a5b9
Rutuja Dond <dondrutuja26@gmail.com>
2023-08-25 10:53:51 +0530

    checking

--- index.js
+++ index.js
@@ -3,12 +3,28 @@ import cors from "cors";
 import path from "path";
 import { fileURLToPath } from "url";
 import { dirname } from "path";
+import fs from "fs";
+import low from "lowdb";
+import FileSync from "lowdb/adapters/FileSync.js"; // Corrected import path
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
+const dbFilePath = path.join(__dirname, "db.json");
+
+// Check if the file exists, if not, create an empty database
+if (!fs.existsSync(dbFilePath)) {
+  fs.writeFileSync(dbFilePath, JSON.stringify({}));
+}
+
+// Create a lowdb instance using the FileSync adapter
+const adapter = new FileSync(dbFilePath);
+const db = low(adapter);
+
+db.set("key", { value: "someValue" }).write(); // Replace "someValue" with the actual value
+
 const server = jsonServer.create();
-const router = jsonServer.router(path.join(__dirname, "db.json"));
+const router = jsonServer.router(dbFilePath);
 const middlewares = jsonServer.defaults();
 
 server.use(cors());
@@ -16,7 +32,7 @@ server.use(jsonServer.bodyParser);
 server.use(middlewares);
 server.use(router);
 
-const PORT = 3000;
+const PORT = 3000; // Hardcoded port number
 
 server.listen(PORT, () => {
   console.log(`JSON Server is running on :${PORT}`);
