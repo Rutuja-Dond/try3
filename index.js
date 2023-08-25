cad31139f5ccca6912cc981f97c6e2ed269c99e8
Rutuja Dond <dondrutuja26@gmail.com>
2023-08-25 15:35:47 +0530

    final commit

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ import cors from "cors";
 import path from "path";
 import { fileURLToPath } from "url";
 import { dirname } from "path";
+import AWS from "aws-sdk";
+
+AWS.config.update({
+  region: "us-east-2",
+  accessKeyId: "ASIA3MWOM5O5UUET53NL",
+  secretAccessKey: "VjU8HcLHsNIa1Ej8yWO8hdFQsjpXQ81GMxZFy8X8",
+});
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -15,6 +22,42 @@ server.use(cors());
 server.use(jsonServer.bodyParser);
 server.use(middlewares);
 server.use(router);
+
+// AWS S3 interaction
+const s3 = new AWS.S3();
+
+// Read and write operations
+server.get("/read-s3", async (req, res) => {
+  try {
+    const s3Response = await s3
+      .getObject({
+        Bucket: "cyclic-lime-brainy-llama-us-east-2",
+        Key: "some_files/my_file.json",
+      })
+      .promise();
+
+    const jsonContent = JSON.parse(s3Response.Body.toString());
+    res.json(jsonContent);
+  } catch (error) {
+    res.status(500).json({ error: "Error reading from S3" });
+  }
+});
+
+server.post("/write-s3", async (req, res) => {
+  try {
+    await s3
+      .putObject({
+        Body: JSON.stringify(req.body),
+        Bucket: "cyclic-lime-brainy-llama-us-east-2",
+        Key: "some_files/my_file.json",
+      })
+      .promise();
+
+    res.json({ message: "Data written to S3" });
+  } catch (error) {
+    res.status(500).json({ error: "Error writing to S3" });
+  }
+});
 
 const PORT = 3000;
 
