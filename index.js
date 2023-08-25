c1a0e82af8a85f3a62a603fab6d3a9d9579ea9b2
Rutuja Dond <dondrutuja26@gmail.com>
2023-08-25 09:43:27 +0530

    last

--- index.js
+++ index.js
@@ -1,32 +1,15 @@
 import jsonServer from "json-server";
 import cors from "cors";
 import path from "path";
-import fs from "fs";
-// const fs = require('fs');
 import { fileURLToPath } from "url";
 import { dirname } from "path";
-
-const data = {
-  key: "value",
-};
-
-const filePath = "./db.json";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const jsonData = JSON.stringify(data, null, 2);
-
 const server = jsonServer.create();
 const router = jsonServer.router(path.join(__dirname, "db.json"));
 const middlewares = jsonServer.defaults();
-
-try {
-  fs.writeFileSync(filePath, jsonData, "utf8");
-  console.log("Data written to file successfully.");
-} catch (err) {
-  console.error("Error writing to file:", err);
-}
 
 server.use(cors());
 server.use(jsonServer.bodyParser);
