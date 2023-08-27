f0e2274e81f799195d12e2a2992e0a92175a5fa7
Rutuja Dond <dondrutuja26@gmail.com>
2023-08-27 15:10:37 +0530

    check

--- index.js
+++ index.js
@@ -1,38 +1,31 @@
-import jsonServer from "json-server";
-import path from "path";
-import { fileURLToPath } from "url";
-import { dirname } from "path";
-
 import express from "express";
+import AWS from "aws-sdk";
 
 const app = express();
 const PORT = 3001;
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 
-// const server = jsonServer.create();
-const router = jsonServer.router(path.join(__dirname, "db.json"));
+AWS.config.update({
+  region: "your-region",
+  accessKeyId: "your-access-key",
+  secretAccessKey: "your-secret-key",
+});
+
+const dynamodb = new AWS.DynamoDB.DocumentClient();
 
 app.use(express.static("public"));
-app.use(router);
+
+app.get("/data", async (req, res) => {
+  try {
+    const params = {
+      TableName: "your-table-name",
+    };
+    const data = await dynamodb.scan(params).promise();
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: "Error reading data" });
+  }
+});
+
 app.listen(PORT, () => {
   console.log("listening on port " + PORT);
 });
-
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = dirname(__filename);
-
-// const server = jsonServer.create();
-// const router = jsonServer.router(path.join(__dirname, "db.json"));
-// const middlewares = jsonServer.defaults();
-
-// server.use(cors());
-// server.use(jsonServer.bodyParser);
-// server.use(middlewares);
-// server.use(router);
-
-// const PORT = 3000;
-
-// server.listen(PORT, () => {
-//   console.log(`JSON Server is running on :${PORT}`);
-// });
