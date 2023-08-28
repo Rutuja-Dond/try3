3e00e2926aeba489c7d826dee5cd1d327845865e
Rutuja Dond <dondrutuja26@gmail.com>
2023-08-28 09:53:00 +0530

    check

--- index.js
+++ index.js
@@ -1,36 +1,60 @@
-import express from "express";
-import AWS from "aws-sdk";
+import jsonServer from "json-server";
+import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
 
-const app = express();
-const PORT = 3001;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-// export AWS_REGION="us-west-1"
-// export AWS_ACCESS_KEY_ID="ASIAYKRRCMGPEVE4MNOB"
-// export AWS_SECRET_ACCESS_KEY="RO8ulhFidIK4roseZsWqDCDKhw0K7zamjdtVZDXX"
-// export AWS_SESSION_TOKEN="IQoJb3JpZ2luX2VjEKr//////////wEaCmFwLXNvdXRoLTEiRzBFAiB9J/sX5kkINKg44TUy5xdaLt5G7h17CLMGbW+sU2218QIhAOxuVKONTevIu8xJvK3ZBZaI/deaDH2qBEEKnmbVVY0NKrQCCHMQABoMNTcyNDA3MzA4NzAyIgx7sBpSL1gfyOrD1d0qkQLRePnPhkbkGVH2tPcBCZ5QV2Ix7tWDBfyMYyOb7ib8swt/WYXgjG5oSUwiBpLFUfLreYa0ZWvhXUE/F42A7qZ8oQA8r0B0sfF+IUOroSe8J4jWdnpvikk6DFUDrqIteG8yKiIhUoAXt4kA56RYi0yILkxQg1ty8h5HV9I2kX9QoImgxlAiI9ACBluIsODF68LYjj/a3K15vDuCsVNz1ij/9RfyQYOiVfhDWzE/diae2Fo5jNvI0quwkkWYP3/Xf736jB/7YVn87PCybBTwWqApoZNz2MpLkvZrGXX9DRFtCfen30hxOsFUuJsoli3j2ifW2xjGJyd4UNBEO9Nl2ME/tRcA15aRqM5k7MIzLUuXxS4w8LWspwY6nQEC1iWLQxmzlJZUQxDOIXdn9uNBxOkkqYb8WNUbVxQ7W0DO+QVj2hXwNKgM+Eq3c+mvChsSAbOxv70fUQL94wTeEVufWAdcTVhftndwkZNEJJZmSJerv/TEtY5Ly6Qaf+7f4a6qI8qZQOGpcvRPTV+z998cnpb/zBTc7qw+eV7JN7P5xx4BdrbsJMawMshSfLFWxU9tF2oarGly6g4A"
+const server = jsonServer.create();
+const router = jsonServer.router(path.join(__dirname, "db.json"));
+const middlewares = jsonServer.defaults();
 
-AWS.config.update({
-  region: "us-west-1",
-  accessKeyId: "ASIAYKRRCMGPEVE4MNOB",
-  secretAccessKey: "RO8ulhFidIK4roseZsWqDCDKhw0K7zamjdtVZDXX",
+server.use(cors());
+server.use(jsonServer.bodyParser);
+server.use(middlewares);
+server.use(router);
+
+const PORT = 3000;
+
+server.listen(PORT, () => {
+  console.log(`JSON Server is running on :${PORT}`);
 });
 
-const dynamodb = new AWS.DynamoDB.DocumentClient();
+// import express from "express";
+// import AWS from "aws-sdk";
 
-app.use(express.static("public"));
+// const app = express();
+// const PORT = 3001;
 
-app.get("/data", async (req, res) => {
-  try {
-    const params = {
-      TableName: "combative-life-jacket-mothCyclicDB",
-    };
-    const data = await dynamodb.scan(params).promise();
-    res.json(data);
-  } catch (error) {
-    res.status(500).json({ error: "Error reading data" });
-  }
-});
+// // export AWS_REGION="us-west-1"
+// // export AWS_ACCESS_KEY_ID="ASIAYKRRCMGPEVE4MNOB"
+// // export AWS_SECRET_ACCESS_KEY="RO8ulhFidIK4roseZsWqDCDKhw0K7zamjdtVZDXX"
+// // export AWS_SESSION_TOKEN="IQoJb3JpZ2luX2VjEKr//////////wEaCmFwLXNvdXRoLTEiRzBFAiB9J/sX5kkINKg44TUy5xdaLt5G7h17CLMGbW+sU2218QIhAOxuVKONTevIu8xJvK3ZBZaI/deaDH2qBEEKnmbVVY0NKrQCCHMQABoMNTcyNDA3MzA4NzAyIgx7sBpSL1gfyOrD1d0qkQLRePnPhkbkGVH2tPcBCZ5QV2Ix7tWDBfyMYyOb7ib8swt/WYXgjG5oSUwiBpLFUfLreYa0ZWvhXUE/F42A7qZ8oQA8r0B0sfF+IUOroSe8J4jWdnpvikk6DFUDrqIteG8yKiIhUoAXt4kA56RYi0yILkxQg1ty8h5HV9I2kX9QoImgxlAiI9ACBluIsODF68LYjj/a3K15vDuCsVNz1ij/9RfyQYOiVfhDWzE/diae2Fo5jNvI0quwkkWYP3/Xf736jB/7YVn87PCybBTwWqApoZNz2MpLkvZrGXX9DRFtCfen30hxOsFUuJsoli3j2ifW2xjGJyd4UNBEO9Nl2ME/tRcA15aRqM5k7MIzLUuXxS4w8LWspwY6nQEC1iWLQxmzlJZUQxDOIXdn9uNBxOkkqYb8WNUbVxQ7W0DO+QVj2hXwNKgM+Eq3c+mvChsSAbOxv70fUQL94wTeEVufWAdcTVhftndwkZNEJJZmSJerv/TEtY5Ly6Qaf+7f4a6qI8qZQOGpcvRPTV+z998cnpb/zBTc7qw+eV7JN7P5xx4BdrbsJMawMshSfLFWxU9tF2oarGly6g4A"
 
-app.listen(PORT, () => {
-  console.log("listening on port " + PORT);
-});
+// AWS.config.update({
+//   region: "us-west-1",
+//   accessKeyId: "ASIAYKRRCMGPEVE4MNOB",
+//   secretAccessKey: "RO8ulhFidIK4roseZsWqDCDKhw0K7zamjdtVZDXX",
+// });
+
+// const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+// app.use(express.static("public"));
+
+// app.get("/data", async (req, res) => {
+//   try {
+//     const params = {
+//       TableName: "combative-life-jacket-mothCyclicDB",
+//     };
+//     const data = await dynamodb.scan(params).promise();
+//     res.json(data);
+//   } catch (error) {
+//     res.status(500).json({ error: "Error reading data" });
+//   }
+// });
+
+// app.listen(PORT, () => {
+//   console.log("listening on port " + PORT);
+// });
